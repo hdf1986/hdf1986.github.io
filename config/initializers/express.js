cc8f4c5fe70c31aee24d2f4572439729fde8cb28
Hugo Farji <hugo@6700.com.ar>
2016-08-03 23:17:29 -0300

    fixes for routes

--- config/initializers/express.js
+++ config/initializers/express.js
@@ -2,5 +2,6 @@ module.exports = function () {
   var _routes = require('../routes')(Mugi.router)
   var routes = _routes.list;
   Express.parseRoutes(routes)
+  Express.static(`${Mugi.projectPath}/public`)
   Express.listen(3000)
 }
