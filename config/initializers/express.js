b8ff145373e98581345c85dc301562d91a19d254
Hugo Farji <hugo@6700.com.ar>
2016-08-04 15:45:21 -0300

    added bower path

--- config/initializers/express.js
+++ config/initializers/express.js
@@ -3,6 +3,7 @@ module.exports = function () {
   var routes = _routes.list;
   Express.parseRoutes(routes)
   Express.static(`${Mugi.projectPath}/public`)
+  Express.static(`${Mugi.projectPath}/vendor/assets`, "/assets")
   Express.app.set('view engine', 'pug');
   Express.app.set('views',`${Mugi.projectPath}/app/views`);
   Express.listen(3000)
